b332893677bd835089594dac35f4719286e8aa6b
shimizu-tnet <70251992+shimizu-tnet@users.noreply.github.com>
2020-09-01 17:01:58 +0900

    add

--- before-install.bat
+++ before-install.bat
@@ -1,6 +1,6 @@
 REM Install Internet Information Server (IIS).
 cd C:\inetpub\wwwroot
 del /q /s *.*
-for /F %a in ('dir /ad /b /w *') do rmdir /S /q %a
+for /F %%a in ('dir /ad /b /w *') do rmdir /S /q %%a
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
