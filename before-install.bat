94410c4465aa8e4b24052251fcd2cede2af446e8
shimizu-tnet <70251992+shimizu-tnet@users.noreply.github.com>
2020-09-02 15:52:07 +0900

    bk

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,6 @@
-REM Install Internet Information Server (IIS).
+mkdir C:\inetpub\BK > NUL 2>&1
+del /q C:\inetpub\BK\*
+powershell -Command "Compress-Archive -Path C:\inetpub\wwwroot -DestinationPath C:\inetpub\bk\%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%.zip"
 cd C:\inetpub\wwwroot
 del /q /s *.*
 for /F %%a in ('dir /ad /b /w *') do rmdir /S /q %%a
