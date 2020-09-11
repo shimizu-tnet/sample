e9d08ff3708a9dcc8d7a0f2ff1738cc3e98c87da
shimizu-tnet <70251992+shimizu-tnet@users.noreply.github.com>
2020-09-11 13:42:51 +0900

    iis

--- after-install.bat
+++ after-install.bat
@@ -1,5 +1,7 @@
+iisreset /stop
 cd C:\inetpub\wwwroot\
 del /q /s *.*
 for /F %%a in ('dir /ad /b /w *') do rmdir /S /q %%a
 xcopy c:\inetpub\deploytmp\JuniorTennis.Mvc\bin\Debug\netcoreapp3.1\publish C:\inetpub\wwwroot /E /I
 rd c:\inetpub\deploytmp /S /Q
+iisreset /start
