6e66a24204d3c84fbd3c1277322dc158a537efd3
shimizu-tnet <70251992+shimizu-tnet@users.noreply.github.com>
2020-10-08 11:25:50 +0900

    Releaseモードパス変更

--- after-install.bat
+++ after-install.bat
@@ -1,8 +1,8 @@
 iisreset /stop
 cd C:\inetpub\wwwroot\
-copy /y web.config c:\inetpub\deploytmp\JuniorTennis.Mvc\bin\Debug\netcoreapp3.1\publish
+copy /y web.config c:\inetpub\deploytmp\JuniorTennis.Mvc\bin\Release\netcoreapp3.1\publish
 del /q /s *.*
 for /F %%a in ('dir /ad /b /w *') do rmdir /S /q %%a
-xcopy c:\inetpub\deploytmp\JuniorTennis.Mvc\bin\Debug\netcoreapp3.1\publish C:\inetpub\wwwroot /E /I
+xcopy c:\inetpub\deploytmp\JuniorTennis.Mvc\bin\Release\netcoreapp3.1\publish C:\inetpub\wwwroot /E /I
 rd c:\inetpub\deploytmp /S /Q
 iisreset /start
